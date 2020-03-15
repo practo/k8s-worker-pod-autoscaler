261f7dced439db0f0029896bf035969b4ff51621
sushant kumar <sushant@exotel.in>
2020-03-15 15:20:09 +0530

    Address review comments

--- pkg/queue/queueing_service.go
+++ pkg/queue/queueing_service.go
@@ -2,6 +2,12 @@ package queue
 
 // QueuingService is the interface for the message queueing service
 // For example: SQS and Beanstalk implements QueuingService interface
+
+const (
+	SqsQueueService       = "sqs"
+	BeanstalkQueueService = "beanstalkd"
+)
+
 type QueuingService interface {
 	Sync(stopCh <-chan struct{})
 	poll(key string, queueSpec QueueSpec)
