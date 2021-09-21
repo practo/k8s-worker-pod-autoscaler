73ba5c45a3ec20f9f2f9e67d59f842e9a7760bbb
Alok Kumar Singh <mail.alok87@gmail.com>
2021-09-21 21:52:10 +0530

    Don't work on unitialized value

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -426,6 +426,15 @@ func (c *Controller) syncHandler(ctx context.Context, event WokerPodAutoScalerEv
 		return nil
 	}
 
+	if queueMessages == queue.UnsyncedQueueMessageCount {
+		klog.Warningf(
+			"%s qMsgs: %d not uninitialized, waiting for init to complete",
+			queueName,
+			queueMessages,
+		)
+		return nil
+	}
+
 	desiredWorkers := GetDesiredWorkers(
 		queueName,
 		queueMessages,
